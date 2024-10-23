a0c70f302c21cdeb87cf024dedba4a38bf337a67
GoDotDotDot <chukui0202@gmail.com>
2024-10-23 22:36:58 +0800

    fix: proxy

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -37,7 +37,7 @@ const nextConfig = {
         return [
             {
                 source: '/api/vector/:path*',
-                destination: 'http://embedding.dctx.link:3000:path*',
+                destination: 'http://embedding.dctx.link:3000/:path*',
             },
         ];
     },
