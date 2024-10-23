a9b3dc7b82bef1554fca1b9867967d0df07d2d9b
GoDotDotDot <chukui0202@gmail.com>
2024-10-23 15:05:15 +0800

    fix: vector

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -33,6 +33,14 @@ const nextConfig = {
             },
         ],
     },
+    async rewrites() {
+        return [
+            {
+                source: '/vector/:path',
+                destination: 'http://embedding.dctx.link:3000/:path/',
+            },
+        ];
+    },
 };
 
 module.exports = withBundleAnalyzer(withNextIntl(withContentlayer(nextConfig)));
